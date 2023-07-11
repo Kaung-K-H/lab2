00426492c7f88e6a03214a27bd8622894c942ada
Kaung Khant Hein <40614799@live.napier.ac.uk>
2023-07-11 13:31:17 +0630

    scanner testing

---
+++ src/main/java/com/napier/sem/test.java
@@ -0,0 +1,12 @@
+package com.napier.sem;
+import java.util.*;
+public class test {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter username");
+
+        String smth = sc.nextLine();
+        System.out.println(smth);
+    }
+}
