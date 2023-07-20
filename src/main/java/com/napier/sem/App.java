87ee528af46c2ca2c09e3fb566fc8c4228214443
Kaung Khant Hein <40614799@live.napier.ac.uk>
2023-07-20 12:01:17 +0630

    I don't even know anymore uhuhfhrtsga g

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -20,12 +20,7 @@ public class App
 
         // Connect to database
         //a.connect();
-        if(args.length < 1){
-            a.connect("localhost:33060", 30000);
-        }else{
-            a.connect(args[0], Integer.parseInt(args[1]));
-        }
-
+        a.connect();
         // Extract employee salary information
         ArrayList<Employee> employees = e.getAllSalaries(a.con, limit);
         e.printSalaries(employees);
@@ -51,7 +46,7 @@ public class App
     /**
      * Connect to the MySQL database.
      */
-    public void connect(String location, int delay) {
+    public void connect() {
         try {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -65,24 +60,21 @@ public class App
             System.out.println("Connecting to database...");
             try {
                 // Wait a bit for db to start
-                Thread.sleep(delay);
+                Thread.sleep(30000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://employees:3306/db?useSSL=false", "root", "example");
-
-                //con = DriverManager.getConnection("jdbc:mysql://" + location
-                //                + "/employees?allowPublicKeyRetrieval=true&useSSL=false",
-                //        "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                // Connect to database in localhost
+                // con = DriverManager.getConnection("jdbc:mysql://localhost:33061/world", "root", "example");
                 System.out.println("Successfully connected");
                 break;
             } catch (SQLException sqle) {
-                System.out.println("Failed to connect to database attempt " +                                  Integer.toString(i));
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
             } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
     }
-
     /**
      * Disconnect from the MySQL database.
      */
